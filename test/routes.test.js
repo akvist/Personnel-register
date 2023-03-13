5263488dad7f92f2b796f8f2f9d5a0d386c3a497
akvist <111497667+akvist@users.noreply.github.com>
2023-03-13 21:49:27 +0100

    POST-test, existing email and new names

--- test/routes.test.js
+++ test/routes.test.js
@@ -28,3 +28,12 @@
       expect(response.statusCode).toBe(201); 
     });
   });
+
+  describe("POST / ", () => {
+    test("An employee should not be added to the array if there already is an employee with the given email, even if the name does not exist in the array", async () => {
+      const response = await request(app).post("/api/personnel/add_personnel").send({name: 'name4', lastName: 'lastName4', email: 'email1'});
+      expect(response.body).not.toContainEqual({name: 'name4', lastName: 'lastName4', email: 'email1'});
+      expect(response.body).toHaveLength(currentNumberOfPersonnel+2);
+      expect(response.statusCode).toBe(405); 
+    });
+  });
