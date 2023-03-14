2f7d571924f833f8ef89da8c0909d8a5f77c9eba
akvist <111497667+akvist@users.noreply.github.com>
2023-03-14 20:34:55 +0100

    Using less code for module.exports of personnel

--- personnel.js
+++ personnel.js
@@ -1,7 +1,5 @@
-let personnel = [
+module.exports = [
     {name: 'name1', lastName: 'lastName1', email: 'email1'}, 
     {name: 'name2', lastName: 'lastName2', email: 'email2'},
     {name: 'name3', lastName: 'lastName3', email: 'email3'}
 ];
-
-module.exports = personnel; 
