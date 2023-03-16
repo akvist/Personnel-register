7431b7d3ef4d071a800f6d0c089fefd8e249b394
akvist <111497667+akvist@users.noreply.github.com>
2023-03-16 22:22:46 +0100

    Tests changed to match urls and personnel id

--- test/routes.test.js
+++ test/routes.test.js
@@ -14,8 +14,8 @@
 
   describe("POST / ", () => {
     test("An employee should be added to the array if there is no already existing employee with the given name, last name and email", async () => {
-      const response = await request(app).post("/personnel").send({name: 'name', lastName: 'lastName', email: 'email'});
-      expect(response.body.result).toContainEqual({name: 'name', lastName: 'lastName', email: 'email'});
+      const response = await request(app).post("/personnel").send({id: 4, name: 'name', lastName: 'lastName', email: 'email'});
+      expect(response.body.result).toContainEqual({id: 4, name: 'name', lastName: 'lastName', email: 'email'});
       expect(response.body.result).toHaveLength(currentNumberOfPersonnel+1);
       expect(response.statusCode).toBe(201); 
     });
@@ -24,7 +24,7 @@
   describe("POST / ", () => {
     test("An employee should be added to the array even if there already is an employee with the given name, but no one with given email", async () => {
       const response = await request(app).post("/personnel").send({name: 'name1', lastName: 'lastName1', email: 'email4'});
-      expect(response.body.result).toContainEqual({name: 'name1', lastName: 'lastName1', email: 'email4'});
+      expect(response.body.result).toContainEqual({id: 5, name: 'name1', lastName: 'lastName1', email: 'email4'});
       expect(response.body.result).toHaveLength(currentNumberOfPersonnel+2);
       expect(response.statusCode).toBe(201); 
     });
@@ -33,7 +33,7 @@
   describe("POST / ", () => {
     test("An employee should not be added to the array if there already is an employee with the given email, even if the name does not exist in the array", async () => {
       const response = await request(app).post("/personnel").send({name: 'name4', lastName: 'lastName4', email: 'email1'});
-      expect(response.body.result).not.toContainEqual({name: 'name4', lastName: 'lastName4', email: 'email1'});
+      expect(response.body.result).not.toContainEqual({id: 6, name: 'name4', lastName: 'lastName4', email: 'email1'});
       expect(response.body.result).toHaveLength(currentNumberOfPersonnel+2);
       expect(response.statusCode).toBe(409); 
     });
@@ -41,7 +41,7 @@
 
   describe("POST / ", () => {
     test("If name, last name or email is not given, no employee will be added", async () => {
-      const response = await request(app).post("/personnel").send({lastName: 'lastName4', email: 'email1'});
+      const response = await request(app).post("/personnel").send({id: 6, lastName: 'lastName4', email: 'email1'});
       expect(response.body.result).toHaveLength(currentNumberOfPersonnel+2);
       expect(response.statusCode).toBe(400); 
     });
@@ -49,7 +49,7 @@
 
   describe("DELETE / ", () => {
     test("An employee should be deleted if there is an employee with the given id", async () => {
-      const response = await request(app).delete("/api/personnel/:id").send(1);
+      const response = await request(app).delete("/api/personnel/1");
       expect(response.body.result).not.toContainEqual({id:1, name: 'name1', lastName: 'lastName1', email: 'email1'});
       expect(response.body.result).toHaveLength(currentNumberOfPersonnel+1);
       expect(response.statusCode).toBe(200); 
@@ -58,8 +58,7 @@
 
   describe("DELETE / ", () => {
     test("An employee should not be deleted if there is no employee with the given id", async () => {
-      const response = await request(app).delete("/api/personnel/:id").send({name: 'name5', lastName: 'lastName5', email: 'email1'});
-      expect(response.body.result).not.toContainEqual({name: 'name5', lastName: 'lastName5', email: 'email1'});
+      const response = await request(app).delete("/api/personnel/7");
       expect(response.body.result).toHaveLength(currentNumberOfPersonnel+1);
       expect(response.statusCode).toBe(404); 
     });
